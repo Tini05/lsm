0f51a4b68de59f34f636059b8e334be87235fd77
Tini05 <artinalimi69@gmail.com>
2026-01-03 18:38:58 +0100

    fixed console error

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,18 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    minify: 'terser',
+    sourcemap: true,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          react: ['react', 'react-dom'],
+          router: ['react-router-dom'],
+          framer: ['framer-motion'],
+          leaflet: ['leaflet', 'react-leaflet'],
+        },
+      },
+    },
+  },
 })
